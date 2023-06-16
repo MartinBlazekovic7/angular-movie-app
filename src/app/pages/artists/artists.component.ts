13013d0564d5c632a33611d98dc02464ebbf522f
Martin Blažeković <martinblazekovic7@gmail.com>
2023-06-16 22:39:54 +0200

    Edited artists tab

--- src/app/pages/artists/artists.component.ts
+++ src/app/pages/artists/artists.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { People } from 'src/app/modules/shared/other/model/people.model';
+import {
+  ArtistDetails,
+  People,
+} from 'src/app/modules/shared/other/model/people.model';
 import { LoaderService } from 'src/app/modules/shared/other/services/loader.service';
 import { PeopleService } from 'src/app/modules/shared/other/services/people.service';
 
@@ -10,7 +13,7 @@ import { PeopleService } from 'src/app/modules/shared/other/services/people.serv
 })
 export class ArtistsComponent implements OnInit {
   popularArtists: People[] = [];
-
+  popularArtistsDetails: ArtistDetails[] = [];
   constructor(
     private peopleService: PeopleService,
     private loaderService: LoaderService
@@ -20,6 +23,11 @@ export class ArtistsComponent implements OnInit {
     this.loaderService.show();
     this.peopleService.getTrending().subscribe((response) => {
       this.popularArtists = response.results;
+      this.popularArtists.map((artist) => {
+        this.peopleService.getDetails(artist.id).subscribe((response) => {
+          this.popularArtistsDetails.push(response);
+        });
+      });
     });
     setInterval(() => {
       this.loaderService.hide();
